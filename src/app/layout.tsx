c7bfad87036c0bd27019c9c33d447d3efb8772a8
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-03-04 13:26:27 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${font.urbanist.className}`}>
+      <body suppressHydrationWarning className={`antialiased ${font.urbanist.className}`}>
         {children}
       </body>
     </html>
